192e2ffc7f4c67c6a12f347eece19b56f2672df3
byteiqbal <iqbalhossen1156@gmail.com>
2025-05-21 02:46:59 +0600

    Image source link fix

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,14 +3,14 @@ export default function Nav() {
     <nav className="bg-navbg rounded-full mt-4 px-8 py-3 flex justify-between items-center">
       <div className="flex items-center ">
         <div className="text-primary mr-2">
-          <img src="./src/assets/logo.svg" />
+          <img src="./assets/logo.svg" />
         </div>
         <h1 className="text-2xl font-bold">
           <span className="text-primary">Dine</span>Out
         </h1>
       </div>
       <div className="flex items-center">
-        <img src="./src/assets/user-icon.svg" className="h-10" />
+        <img src="./assets/user-icon.svg" className="h-10" />
       </div>
     </nav>
   );
